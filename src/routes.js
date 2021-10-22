929d653977132bab346116b1fa724f2c47c13f4c
namdk1028 <namdk7@naver.com>
2021-10-22 10:10:27 +0900

    feature D005 | 송금 페이지 세팅

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import Test from './components/HelloWorld.vue'
 import Reservation from './views/contract/Reservation.vue'
 import Loan from './views/deal/Loan.vue'
 import Procedure from "./views/deal/Procedure.vue"
+import Remit from "./views/deal/Remit.vue"
 import Contract from './views/contract/Contract.vue'
 
 export default [
@@ -62,6 +63,11 @@ export default [
         path: "/loan/procedure",
         name: "Procedure",
         component: Procedure
+    },
+    {
+        path: "/remit",
+        name: "Remit",
+        component: Remit
     },
     {
         path: '/contract',
---
+++ src/views/deal/Remit.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="remit">
+        <div class='remit'>
+            <div class='title'>
+                송금하기
+            </div>
+            <div class="content">
+                <v-card class="remit-box sender">
+                    <h5>출금 계좌번호</h5>
+                    <v-avatar size='1.5rem'>
+                        <img
+                            src="../../assets/images/kblogo.png"
+                        >
+                    </v-avatar>
+                </v-card>
+                <v-card class="remit-box recipient">
+
+                </v-card>
+                <v-card class="remit-box amount">
+
+                </v-card>
+                <v-card class="remit-box sign">
+
+                </v-card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+#remit {
+    height: 100vh;
+}
+.remit {
+    height: 100vh;
+
+}
+#remit .title {
+    height: 8vh;
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: gainsboro;
+}
+#remit .content {
+    height: 92vh;
+    padding: 20px;
+}
+@media screen and (min-width: 900px) {
+  .remit {
+    max-width: 580px;
+    margin: auto;
+  }
+}
+.remit-box {
+    margin-top: 2vh;
+    height: 18vh;
+}
+</style>
