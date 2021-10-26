b59a7a406497f72e04c4925b59fd89ebaf01035e
namdk1028 <namdk7@naver.com>
2021-10-26 13:57:59 +0900

    feature C003 | 서명란

--- src/components/contract/DrawingCanvas.vue
+++ src/components/contract/DrawingCanvas.vue
@@ -1,23 +1,33 @@
 <template>
     <div class="drawing">
+        <div class='sign'>
+            <div>
+                <span>임차인</span>
 
-        <vue-drawing-canvas
-            ref="VueCanvasDrawing"
-            class='canvas'
-            width="200"
-            height="140"
-            margin="0"
-            :lock="disabled"
-        />
-        <div class="canvas-btns">
-            <v-btn @click="reset">초기화</v-btn>
+                <v-img class="lessor-sign" :src="lessor.sign">
+                </v-img>
+                
+            </div>
+            <div>
+                <span>임대인</span>
+                <vue-drawing-canvas
+                    ref="VueCanvasDrawing"
+                    class='canvas'
+                    width="150"
+                    height="80"
+                    margin="0"
+                    :lock="disabled"
+                />
+                <v-btn @click="reset">초기화</v-btn>
 
-            <v-btn @click="save">
-                <span v-if="!disabled">완료</span>
-                <span v-else>수정</span>
-            </v-btn>
+                <v-btn @click="save">
+                    <span v-if="!disabled">완료</span>
+                    <span v-else>수정</span>
+                </v-btn>
+            </div>
         </div>
-        <v-img :src="user.sign" style="max-height: 60px; max-width: 80px;"></v-img>
+        
+        <v-img :src="user.sign"></v-img>
     </div>
 </template>
 
@@ -37,16 +47,19 @@ export default {
         user() {
             return this.$store.state.user
         },
+        lessor() {
+            return this.$store.state.lessor
+        },
     },
     methods: {
         reset() {
-            console.log(this.image)
-            this.$refs.VueCanvasDrawing.reset()
-            console.log("reset")
         },
         save() {
             var canvas = document.querySelector('.canvas')
             this.$store.state.user.sign = canvas.toDataURL()
+            if (this.disabled) {
+                this.$refs.VueCanvasDrawing.reset()
+            }
             this.disabled = !this.disabled
         }
     }
@@ -56,7 +69,16 @@ export default {
 <style>
 .drawing {
     display: block;
-    width: 204px;
+    width: 330px;
+}
+.sign {
+    display: flex;
+}
+.lessor-sign {
+    border: solid 2px;
+    width: 150px;
+    height: 84px;
+    margin-right: 22px;
 }
 .canvas {
     border: solid 2px;
